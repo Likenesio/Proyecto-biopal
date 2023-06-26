14f499e85b6ffb04f70a338174403a068dccf429
likenesio <valentinubb@gmail.com>
2023-06-25 20:59:18 -0400

    [routes en el navbar]

--- Frontend/biopal/src/app/components/navbar/navbar.component.ts
+++ Frontend/biopal/src/app/components/navbar/navbar.component.ts
@@ -48,10 +48,44 @@ export class NavbarComponent implements OnInit{
             icon: 'pi pi-fw pi-trash',
             routerLink:['cliente/crear']
 
-             }
+             },
             ]
 
         },
+        {
+          label: 'Usuario',
+          icon: 'pi pi-fw pi-user',
+          items: [
+            {
+              label: 'Ingresar Nuevo Usuario',
+              icon: 'pi pi-fw pi-trash',
+              routerLink:['usuario/crear']
+
+
+            },
+
+              {
+                  label: 'Search',
+                  icon: 'pi pi-fw pi-users',
+                  items: [
+                      {
+                          label: 'Filter',
+                          icon: 'pi pi-fw pi-filter',
+                          items: [
+                              {
+                                  label: 'Print',
+                                  icon: 'pi pi-fw pi-print'
+                              }
+                          ]
+                      },
+                      {
+                          icon: 'pi pi-fw pi-bars',
+                          label: 'List'
+                      }
+                  ]
+              }
+          ]
+      },
         {
           label: 'Quit',
           icon: 'pi pi-fw pi-power-off',
@@ -84,6 +118,59 @@ export class NavbarComponent implements OnInit{
               routerLink:['productos/ingresar']
 
              }
+
+       ]
+       },
+       {
+        label: 'Pedidos',
+        icon: 'pi pi-fw pi-user',
+        items: [
+            {
+                label: 'New',
+                icon: 'pi pi-fw pi-user-plus'
+            },
+            {
+                label: 'Delete',
+                icon: 'pi pi-fw pi-user-minus'
+            },
+            {
+                label: 'Search',
+                icon: 'pi pi-fw pi-users',
+                items: [
+                    {
+                        label: 'Filter',
+                        icon: 'pi pi-fw pi-filter',
+                        items: [
+                            {
+                                label: 'Print',
+                                icon: 'pi pi-fw pi-print'
+                            }
+                        ]
+                    },
+                    {
+                        icon: 'pi pi-fw pi-bars',
+                        label: 'List'
+                    }
+                ]
+            }
+        ]
+    },
+
+      {
+        label: 'Quit',
+        icon: 'pi pi-fw pi-power-off',
+        command: ()  => this.logout()
+      }
+
+        ]}
+
+    if(this.authService.obtenerRol() =="Facturador"){
+        this.items = [ {
+
+          label: 'Documentos',
+          icon: 'pi pi-fw pi-file',
+          items: [
+
        ]
 
 
@@ -94,128 +181,25 @@ export class NavbarComponent implements OnInit{
         command: ()  => this.logout()
       }
 
-        ]
-    }
+        ]}
+        if(this.authService.obtenerRol() =="Bodeguero"){
+            this.items = [ {
+
+              label: 'Inventario',
+              icon: 'pi pi-fw pi-file',
+              items: [
+
+           ]
 
 
-        this.items = [
+          },
+          {
+            label: 'Quit',
+            icon: 'pi pi-fw pi-power-off',
+            command: ()  => this.logout()
+          }
 
-            {
-
-                label: 'Inventario Productos',
-                icon: 'pi pi-fw pi-file',
-                items: [
-                    {
-                        label: 'Listar Productos',
-                        icon: 'pi pi-fw pi-plus',
-                        routerLink:['productos/listar']
-
-                    },
-                    {
-                    label: 'Actualizar Producto Exitente',
-                    icon: 'pi pi-fw pi-trash',
-                    routerLink:['productos/actualizar']
-
-                   },
-                   {
-                    label: 'Ingresar Nuevo Producto',
-                    icon: 'pi pi-fw pi-trash',
-                    routerLink:['productos/ingresar']
-
-                   }
-            ]
-
-
-            },
-
-            {
-                label: 'Clientes',
-                icon: 'pi pi-fw pi-pencil',
-                items: [
-                  {
-                    label: 'Listar Clientes',
-                    icon: 'pi pi-fw pi-plus',
-                    routerLink:['cliente/listar']
-
-                },
-                {
-                label: 'Actualizar Cliente Existente',
-                icon: 'pi pi-fw pi-trash',
-                routerLink:['cliente/update']
-
-               },
-               {
-                label: 'Ingresar Nuevo Cliente',
-                icon: 'pi pi-fw pi-trash',
-                routerLink:['cliente/crear']
-
-                 }
-                ]
-            },
-            {
-                label: 'Pedidos',
-                icon: 'pi pi-fw pi-user',
-                items: [
-                    {
-                        label: 'New',
-                        icon: 'pi pi-fw pi-user-plus'
-                    },
-                    {
-                        label: 'Delete',
-                        icon: 'pi pi-fw pi-user-minus'
-                    },
-                    {
-                        label: 'Search',
-                        icon: 'pi pi-fw pi-users',
-                        items: [
-                            {
-                                label: 'Filter',
-                                icon: 'pi pi-fw pi-filter',
-                                items: [
-                                    {
-                                        label: 'Print',
-                                        icon: 'pi pi-fw pi-print'
-                                    }
-                                ]
-                            },
-                            {
-                                icon: 'pi pi-fw pi-bars',
-                                label: 'List'
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                label: 'Usuaios',
-                icon: 'pi pi-fw pi-calendar',
-                items: [
-                    {
-                      label: 'Ingresar Nuevo Usuario',
-                      icon: 'pi pi-fw pi-trash',
-                      routerLink:['usuario/crear']
-
-
-                    },
-                    {
-                        label: 'Archieve',
-                        icon: 'pi pi-fw pi-calendar-times',
-                        items: [
-                            {
-                                label: 'Remove',
-                                icon: 'pi pi-fw pi-calendar-minus'
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                label: 'Quit',
-                icon: 'pi pi-fw pi-power-off',
-                command: ()  => this.logout()
-            }
-        ];
-    }
+            ]}
 }
-
+}
 
