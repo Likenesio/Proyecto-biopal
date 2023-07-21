f8964c501d66a5d149278169f32a24a0210e526f
likenesio <valentinubb@gmail.com>
2023-07-20 22:22:53 -0400

    [carrossel home]

--- Frontend/src/app/components/home/home/home.component.ts
+++ Frontend/src/app/components/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  images = [
+    '/assets/1.png',
+    '/assets/2.png',
+    '/assets/3.png',
+    '/assets/4.png',
+    '/assets/5.png',
+    // Agrega aquí más imágenes si es necesario
+  ];
+
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 4000; // Cambiar de imagen cada 5 segundos
+    config.wrap = true; // Reiniciar al principio después de llegar al final
+    config.keyboard = false; // Deshabilitar navegación con teclado
+    config.pauseOnHover = false; // No pausar el carrusel al pasar el ratón sobre él
+  }
 
   ngOnInit() {
   }
