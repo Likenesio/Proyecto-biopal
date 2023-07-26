f9235f5806ae99c431c39bc5945c1e35520e7e78
likenesio <valentinubb@gmail.com>
2023-07-26 15:33:17 -0400

    [Models eliminados]

--- Frontend/biopal/models/pedido.ts
+++
@@ -1,5 +0,0 @@
-export interface Pedido {
-  numero_pedido: number;
-  cliente: string; // Puedes cambiar el tipo según el modelo de cliente
-  usuario: string; // Puedes cambiar el tipo según el modelo de usuario
-}
--- Frontend/models/pedido-producto.ts
+++
@@ -1,6 +0,0 @@
-export interface PedidoProducto {
-  producto: string; // Puedes cambiar el tipo según el modelo de producto
-  pedido: string; // Puedes cambiar el tipo según el modelo de pedido
-  cantidad_producto: number;
-  subtotal: number;
-}
