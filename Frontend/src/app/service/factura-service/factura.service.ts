313ca74fc20a854eec59a4ed96167a7474066425
likenesio <valentinubb@gmail.com>
2023-08-01 13:03:30 -0400

    [factura service]

---
+++ Frontend/src/app/service/factura-service/factura.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BoletaService {
+  private apiUrl = 'http://localhost:3000/api/factura';
+
+  constructor(private http: HttpClient) { }
+
+  crearBoleta(factura: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, factura);
+  }
+
+  buscarFacturaID(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+  buscarFacturaNumero(numero: any): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/buscar/${numero}`);
+  }
+  listarFacturas(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+  eliminarFactura(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  }
+
+  actualizarFactura(id: string, factura: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${id}`, factura);
+  }
+}
