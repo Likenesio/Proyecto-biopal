68da5f2e57cf2c35d852604895ee8b3b3d56705a
likenesio <valentinubb@gmail.com>
2023-08-14 20:35:34 -0400

    [verificar si tiene pedidos asociados un cliente]

--- Frontend/src/app/components/Cliente/listar-cliente/listar-cliente/listar-cliente.component.ts
+++ Frontend/src/app/components/Cliente/listar-cliente/listar-cliente/listar-cliente.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ClienteService } from 'src/app/service/cliente-service/cliente.service';
+import { PedidoService } from 'src/app/service/pedido/pedido.service';
 import Swal from 'sweetalert2';
 
 export interface Cliente{
@@ -23,9 +24,11 @@ export class ListarClienteComponent {
   listarClientes: Cliente[] = [];
   first = 0;
   rows = 10;
+  pedidos:any;
+  idCliente: any[]=[];
 
 
-  constructor(private clienteService: ClienteService) { }
+  constructor(private clienteService: ClienteService, private pedidoService: PedidoService) { }
 
   ngOnInit() {
      this.clienteService.listarClientes().subscribe(data=>{
@@ -46,34 +49,74 @@ export class ListarClienteComponent {
         });
       })
      })
+     
 
 
+this.pedidoService.listarP().subscribe((data) => {
+  this.pedidos = data.pedido;
+  console.log("Pedidos: ", this.pedidos);
+
+  // Crear un array para almacenar los IDs de los clientes
+  const clientesIDs = [];
+
+  // Iterar a través de los pedidos
+  for (const pedido of this.pedidos) {
+    // Verificar si hay clientes en el pedido
+    if (pedido.cliente && pedido.cliente.length > 0) {
+      // Obtener el ID del primer cliente en el pedido
+      const clienteID = pedido.cliente[0]._id;
+      clientesIDs.push(clienteID); // Agregar el ID a la matriz
+    }
   }
 
-  eliminar(id: string) {
-    Swal.fire({
-      title: 'Estas seguro de eliminar al cliente?',
-      text: "No podras revertirlo!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Eliminar!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.clienteService.eliminarCliente(id).subscribe(data => {
-        Swal.fire(
-          'Eliminado!',
-          'El cliente ha sido eliminado.',
-          'success'
-        )
-        setTimeout(() => {
-          window.location.reload();
-        }, 800);
-       });
-      }
+  console.log("IDs de Clientes: ", clientesIDs);
+});
+
+
+}
+
+eliminar(id: string) {
+  // Primero, verifica si el cliente tiene pedidos asociados
+  this.pedidoService.listarP().subscribe((data) => {
+    const pedidos = data.pedido;
+
+    // Verificar si hay algún pedido asociado al cliente
+    const tienePedidosAsociados = pedidos.some((pedido:any) => {
+      return pedido.cliente[0]._id === id;
     });
-  }
+
+    if (tienePedidosAsociados) {
+      // Mostrar un mensaje de error indicando que el cliente tiene pedidos asociados
+      Swal.fire({
+        title: 'No se puede eliminar el cliente',
+        text: 'El cliente tiene pedidos asociados.',
+        icon: 'error',
+      });
+    } else {
+      // Mostrar el cuadro de confirmación de eliminación
+      Swal.fire({
+        title: '¿Estás seguro de eliminar al cliente?',
+        text: '¡No podrás revertirlo!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Eliminar',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // Realizar la eliminación del cliente
+          this.clienteService.eliminarCliente(id).subscribe((data) => {
+            Swal.fire('Eliminado', 'El cliente ha sido eliminado.', 'success');
+            setTimeout(() => {
+              window.location.reload();
+            }, 800);
+          });
+        }
+      });
+    }
+  });
+}
+
 
   next() {
     this.first = this.first + this.rows;
